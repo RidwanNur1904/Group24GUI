ed9bad1f797440bb188eb539f3a0c4e3d4b69120
RidwanNur1904 <ridwan.nur@city.ac.uk>
2023-03-29 13:37:22 +0100

    stylised Admin Options and added extra security features to the code

--- src/AdminOptions.java
+++ src/AdminOptions.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class AdminOptions extends JFrame {
 
     private JPanel AOpanel;
+    private JButton updateBlanksButton;
+    private JButton repairDatabaseButton;
+    private JButton removeUserButton;
+    private JLabel AdminImage;
 
     AdminOptions(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -10,5 +15,12 @@ public class AdminOptions extends JFrame {
         this.setTitle("Admin Options");
         this.setLocationRelativeTo(null); // set location to center of the screen
         this.setContentPane(AOpanel);
+
+        ImageIcon originalIcon = new ImageIcon("data/Admin.png"); // Replace this with the actual path to your image file
+        Image originalImage = originalIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        AdminImage.setIcon(scaledIcon);
+
     }
 }
