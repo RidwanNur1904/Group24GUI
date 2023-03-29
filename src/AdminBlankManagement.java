712b9b137983f9cc49fef3895425790cc94e5870
RidwanNur1904 <ridwan.nur@city.ac.uk>
2023-03-29 17:15:32 +0100

    connected the blank management frame to the admin options form as well as made it possible to navigate between the pages

--- src/AdminBlankManagement.java
+++ src/AdminBlankManagement.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AdminBlankManagement extends JFrame {
     private JPanel ABMpanel;
+    private JButton checkStocklistButton;
+    private JButton editReportsButton;
+    private JButton requestBlanksButton;
+    private JButton sendBlanksButton;
+    private JButton back;
+    private JLabel AdminImage;
 
     AdminBlankManagement(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -9,5 +18,27 @@ public class AdminBlankManagement extends JFrame {
         this.setTitle("Admin Blank Manager");
         this.setLocationRelativeTo(null); // set location to center of the screen
         this.setContentPane(ABMpanel);
+
+        ImageIcon originalIcon = new ImageIcon("data/Admin.png"); // Replace this with the actual path to your image file
+        Image originalImage = originalIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        AdminImage.setIcon(scaledIcon);
+
+        ImageIcon logoutIcon = new ImageIcon("data/Back.png"); // Replace this with the actual path to your logout image file
+        Image logoutImage = logoutIcon.getImage();
+        Image scaledLogoutImage = logoutImage.getScaledInstance(45, 45, Image.SCALE_SMOOTH);
+        ImageIcon scaledLogoutIcon = new ImageIcon(scaledLogoutImage);
+        back.setIcon(scaledLogoutIcon); // Set the icon of the JLabel to the scaled logout image
+
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AdminOptions adminOptions = new AdminOptions();
+                dispose();
+                adminOptions.setVisible(true);
+            }
+        });
+
     }
 }
