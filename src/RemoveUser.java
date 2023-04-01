2be489a90b3c9f5971a09e03403c6b323dc38110
RidwanNur1904 <ridwan.nur@city.ac.uk>
2023-04-01 18:01:31 +0100

    Added the layout for the User removal frame

--- src/RemoveUser.java
+++ src/RemoveUser.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RemoveUser extends JFrame {
     private JPanel RUpanel;
     private JLabel AdminImage;
+    private JComboBox UserList;
+    private JButton RRbutton;
+    private JButton exitButton;
+    private JTextField Reason;
+    private JLabel removallabel;
 
     RemoveUser(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,5 +24,21 @@ public class RemoveUser extends JFrame {
         Image scaledImage = originalImage.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
         AdminImage.setIcon(scaledIcon);
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                AdminOptions adminOptions = new AdminOptions();
+                adminOptions.setVisible(true);
+            }
+        });
+
+        RRbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
