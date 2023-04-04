1739c306ba71e18c1b9479e425f9d80c4eb72131
Blizztewa <91624383+Blizztewa@users.noreply.github.com>
2023-04-04 13:21:44 +0100

    Added functionality to the ReallocateBlanks so that the drop down menu displays the correct information

--- src/ReallocateBlanks.java
+++ src/ReallocateBlanks.java
@@ -3,15 +3,16 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class ReallocateBlanks extends JFrame {
     private JPanel ReallocatePanel;
     private JLabel BlankReallocation;
     private JLabel OfficeManagerImage;
     private JButton back;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
+    private JComboBox InitialTA;
+    private JComboBox AllocatedBlanks;
+    private JComboBox FinalTA;
     private JButton ReallocateBlanksButton;
     private JTextField Date;
 
@@ -40,6 +41,61 @@ public class ReallocateBlanks extends JFrame {
 
         Border border = BorderFactory.createMatteBorder(0, 0, 5, 0, Color.BLACK); // Create black bottom border
         Date.setBorder(border);
+
+        //shows the Usernames from the MySQL table TravelAdvisor
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g24", "in2018g24_a", "GTrSnz41");
+
+            // Retrieve the Username values from the TravelAdvisor table
+            PreparedStatement stmt = con.prepareStatement("SELECT DISTINCT Username FROM allocatedBlanks");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String username = rs.getString("Username");
+                InitialTA.addItem(username);
+            }
+            con.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        // shows the Usernames from the MySQL table TravelAdvisor for the FinalTA JComboBox
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g24", "in2018g24_a", "GTrSnz41");
+
+            // Retrieve the Username values from the TravelAdvisor table
+            PreparedStatement stmt = con.prepareStatement("SELECT DISTINCT Username FROM TravelAdvisor");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String username = rs.getString("Username");
+                FinalTA.addItem(username); // add username to FinalTA JComboBox
+            }
+            con.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        // shows the BlankID values associated with the selected Username from the allocatedBlanks table
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g24", "in2018g24_a", "GTrSnz41");
+
+            // Retrieve the BlankID values associated with the selected Username from the allocatedBlanks table
+            String selectedUsername = (String) InitialTA.getSelectedItem(); // get the selected Username
+            PreparedStatement stmt = con.prepareStatement("SELECT BlankID FROM allocatedBlanks WHERE Username = ?");
+            stmt.setString(1, selectedUsername); // set the parameter to the selected Username
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String blankID = rs.getString("BlankID");
+                AllocatedBlanks.addItem(blankID); // add BlankID to AllocatedBlanks JComboBox
+            }
+            con.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+
 
         back.addActionListener(new ActionListener() {
             @Override
