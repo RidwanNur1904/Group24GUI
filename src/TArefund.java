4c711f82862074502a2f3a8a72fea29aa82cb9ca
RidwanNur1904 <ridwan.nur@city.ac.uk>
2023-04-08 19:05:50 +0100

    Finished the refund frame completely.

--- src/TArefund.java
+++ src/TArefund.java
@@ -83,7 +83,78 @@ public class TArefund extends JFrame {
         initiateRefundButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Retrieve selected BlankID value
+                String selectedBlankId = (String) BlankList.getSelectedItem();
 
+                // Establish connection to MySQL database
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g24",
+                            "in2018g24_a", "GTrSnz41"); // Replace with your actual database connection details
+
+                    // Query the SoldBlanks table to retrieve the data for the selected BlankID
+                    String selectQuery = "SELECT * FROM SoldBlanks WHERE BlankID = ?";
+                    PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
+                    selectStatement.setString(1, selectedBlankId);
+                    ResultSet resultSet = selectStatement.executeQuery();
+
+                    if (resultSet.next()) {
+                        // Retrieve the values from the SoldBlanks table
+                        String username = resultSet.getString("username");
+                        String customerFirst = resultSet.getString("customerFirst");
+                        String customerLast = resultSet.getString("customerLast");
+                        String locationFrom = resultSet.getString("locationFrom");
+                        String locationTo = resultSet.getString("locationTo");
+                        String paymentMethod = resultSet.getString("paymentMethod");
+                        String total = resultSet.getString("Total");
+                        String email = resultSet.getString("Email");
+                        Date date = resultSet.getDate("Date");
+                        String paidStatus = resultSet.getString("paidStatus");
+
+                        // Insert the retrieved data into the refundprocessing table
+                        String insertQuery = "INSERT INTO refundprocessing (username, BlankID, customerFirst, customerLast, locationFrom, locationTo, paymentMethod, Total, Email, Date, paidStatus) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                        PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
+                        insertStatement.setString(1, username);
+                        insertStatement.setString(2, selectedBlankId);
+                        insertStatement.setString(3, customerFirst);
+                        insertStatement.setString(4, customerLast);
+                        insertStatement.setString(5, locationFrom);
+                        insertStatement.setString(6, locationTo);
+                        insertStatement.setString(7, paymentMethod);
+                        insertStatement.setString(8, total);
+                        insertStatement.setString(9, email);
+                        insertStatement.setDate(10, date);
+                        insertStatement.setString(11, paidStatus);
+                        insertStatement.executeUpdate();
+
+                        // Remove the selected BlankID from the SoldBlanks table
+                        String deleteQuery = "DELETE FROM SoldBlanks WHERE BlankID = ?";
+                        PreparedStatement deleteStatement = connection.prepareStatement(deleteQuery);
+                        deleteStatement.setString(1, selectedBlankId);
+                        deleteStatement.executeUpdate();
+
+                        JOptionPane.showMessageDialog(null, "The refund process has now started.");
+
+                        deleteStatement.close();
+                        insertStatement.close();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "No data found for selected BlankID in SoldBlanks.");
+                    }
+
+                    resultSet.close();
+                    selectStatement.close();
+                    connection.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                TravelAdvisorOptions travelAdvisorOptions = new TravelAdvisorOptions();
+                travelAdvisorOptions.setVisible(true);
             }
         });
 
